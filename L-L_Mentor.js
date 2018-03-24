37e8f9d6e32e8fa716f61ceb6dfb461fd129e1e8
Jump-Suit <JumpSuitGamer@gmail.com>
2018-03-24 18:27:39 -0500

    Update L-L_Mentor.js

--- L-L_Mentor.js
+++ L-L_Mentor.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./botconfig.json");
 const fs = require("fs");
+const cmds = require("cmds");
+
 client.login(config.token);
 
 client.commands = new Discord.Collection();
