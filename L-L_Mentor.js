abe7ad0be1c01764d8a550bdcf0fffd4234bae11
Jump-Suit <JumpSuitGamer@gmail.com>
2018-03-25 16:32:44 -0500

    Update L-L_Mentor.js

--- L-L_Mentor.js
+++ L-L_Mentor.js
@@ -51,22 +51,6 @@ client.on("message", async message => {
     })
 });
 
-client.on("message", async message => { // Kick command
-    if (command === "kick") {
-        if (message.member.roles.some(r => ["Administrator", "Moderator"].includes(r.name)))
-            return message.reply("Sorry, you don't have permissions to use this!");
-        let member = message.mentions.members.first();
-        if (!member)
-            return message.reply("Please mention a valid member of this server");
-        if (!member.kickable)
-            return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-        let reason = args.slice(1).join(' ');
-        if (!reason)
-            return message.reply("Please indicate a reason for the kick!");
-        await member.kick(reason)
-        console.catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-        message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-    });
 
 client.on('message', message => { // add/remove StarMade role
     if (message.content === (config.prefix + "addstarmade")) {
