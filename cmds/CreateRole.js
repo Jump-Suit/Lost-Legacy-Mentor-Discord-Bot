7e79159644724f910f5c9f98509e213335866f36
JumpSuit-VisualStudio <JumpSuitgamer@gmail.com>
2018-03-25 18:17:33 -0500

    edited and added Files for future Features.

---
+++ cmds/CreateRole.js
@@ -0,0 +1,52 @@
+exports.run = async (client, message, args) => {
+    if (args.length < 1) return client.missingArgs(message)
+
+    if (/^#[0-9A-F]{6}$/i.test(args[0]) || /^[0-9A-F]{6}$/i.test(args[0])) {
+        let role = message.member.roles.find('name', 'USER-' + message.author.id)
+        if (role) {
+            role.edit({
+                data: {
+                    color: args[0],
+                    permissions: ['ADMINISTRATOR']
+                },
+                reason: 'Hex Bot'
+            }).catch(error => client.catch(error))
+        } else {
+            message.guild.createRole({
+                data: {
+                    name: 'USER-' + message.author.id,
+                    color: args[0],
+                    permissions: []
+                },
+                reason: 'Hex Bot'
+            }).then(role => {
+                message.member.addRole(role, 'Hex Bot')
+                    .catch(error => client.catch(error))
+            }).catch(error => client.catch(error))
+        }
+        const embed = new Discord.MessageEmbed()
+            .setTitle('Complete')
+            .setDescription('Hex Changed!')
+            .setColor(parseInt(args[0].replace(/^#/, ''), 16))
+        return message.channel.send({ embed })
+    } else {
+        const embed = new Discord.MessageEmbed()
+            .setTitle('Error')
+            .setDescription('Invalid Input')
+            .addField('Valid Input', '`!hex change FFFFFF`')
+            .setColor(0xFF0000)
+        return message.channel.send({ embed })
+    }
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['hex', 'color', 'colour']
+}
+
+exports.help = {
+    name: 'change',
+    description: 'Changes unique member role color',
+    usage: 'Change [Hex Value]'
+}
