fd52d31582e15b512e7791c5116753832fea9e83
JumpSuit-VisualStudio <JumpSuitgamer@gmail.com>
2018-03-30 17:14:51 -0500

    console.log failed removed.

--- L-L_Mentor.js
+++ L-L_Mentor.js
@@ -23,8 +23,7 @@ fs.readdir("./cmds/", (err, files) => {
     jsfiles.forEach((f, i) => {
         let props = require(`./cmds/${f}`);
         console.log(`$(i + 1}: ${f} loaded!`);
-        client.commands.set(props.help.name, props).catch(console.error);
-
+        client.commands.set(props.help.name, props);
     });
 });
 
