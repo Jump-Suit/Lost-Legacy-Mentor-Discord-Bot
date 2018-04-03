56eee3c90fe7496e3ce8709c3cad82d00f1bc38e
JumpSuit-VisualStudio <JumpSuitgamer@gmail.com>
2018-04-03 10:47:48 -0500

    addded single and double quotes in eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
         ],
         "quotes": [
             "error",
-            "double"
+            "double", "single", 
         ],
         "semi": [
             "error",
