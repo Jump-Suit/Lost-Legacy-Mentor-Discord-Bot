5d3fe8dc9a367b098d0509b8167662dc55505e39
JumpSuit-VisualStudio <JumpSuitgamer@gmail.com>
2018-04-03 05:47:20 -0500

    reset no-console to off.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     "parser": "babel-eslint",
     "rules": {
-        "no-console": 0,
+        "no-console": "off",
         "strict": 0,
         "indent": [
             "error",
